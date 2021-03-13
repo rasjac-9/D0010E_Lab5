fbecc03a3f675b9dee970a02db826f8660e50675
rasjac-9 <ragge@MSI.local>
2021-03-13 12:50:04 +0100

    Changes to all EVENT

--- Lab5/src/Lab5/event/StopEvent.java
+++ Lab5/src/Lab5/event/StopEvent.java
@@ -3,7 +3,6 @@ package Lab5.event;
 
 import Lab5.generic.Event;
 import Lab5.generic.State;
-import Lab5.generic.EventQueue;
 import Lab5.specifid.SuperMarket;
 
 /**
@@ -18,16 +17,14 @@ import Lab5.specifid.SuperMarket;
 public class StopEvent extends Event {
 
 	/**
-	 * Constructor
+	 * Constructor that creates the stop event
 	 * 
-	 * @param eventQ   - EventQueue
 	 * @param sm       - SuperMarket
 	 * @param stopTime - Event time
 	 */
-	public StopEvent(EventQueue eventQ, State sm, double stopTime) {
+	public StopEvent(State sm, double stopTime) {
 		
 		// TODO Auto-generated constructor stub
-		eq = eventQ;
 		s = sm;
 		time = stopTime;
 		name = "Stop";
@@ -37,8 +34,6 @@ public class StopEvent extends Event {
 	 * Changes the run boolean to false
 	 */
 	public void effect() {
-//		System.out.println("******************************************************************************");
-		
 		((SuperMarket)s).stopUpdate(this);
 		s.stopSim();
 	} 
