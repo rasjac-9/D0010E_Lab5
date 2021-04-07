e4a34d8206021f51b0f2d33da517ab7e91d84b5a
rasjac-9 <ragge@MSI>
2021-04-07 13:51:07 +0200

    Delete java dock

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Lab5.Main"}];updateSearchResults();
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"Lab5.Main","l":"RunSim"}];updateSearchResults();
